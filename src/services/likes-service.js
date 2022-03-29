4d67fd698d77477f3e0e354004fc1aa93e430e53
tgreizer <Microbirdman159>
2022-03-29 14:19:26 -0400

    fro work

---
+++ src/services/likes-service.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_URL = "https://foundation-a4.herokuapp.com";
+const TUITS_API = "https://foundation-server-a1.herokuapp.com/tuits";
+const USERS_API = "https://foundation-server-a1.herokuapp.com/users";
+
+const api = axios.create({
+  withCredentials: true
+});
+
+export const findAllTuitsLikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/likes`)
+    .then(response => response.data);
+
+export const findAllUsersThatLikedTuit = (tid) =>
+    api.get(`${TUITS_API}/${tid}/likes`)
+    .then(response => response.data);
+
+export const userLikesTuit = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/likes/${tid}`)
+    .then(response => response.data);
