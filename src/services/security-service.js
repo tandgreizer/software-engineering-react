9c7d4151a71e7a5aa22b3e126a274b7a0587d4ad
tgreizer <Microbirdman159>
2022-03-29 12:11:32 -0400

    fro work

---
+++ src/services/security-service.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+ const BASE_URL = "https://foundation-a4.herokuapp.com";
+// const BASE_URL = "http://localhost:4000/api";
+
+
+const SECURITY_API = `${BASE_URL}/api/auth`;
+
+const api = axios.create({
+  withCredentials: true
+});
+
+export const register = (user) =>
+    api.post(`${SECURITY_API}/register`, user)
+    .then(response => response.data);
+
+export const login = (user) =>
+    api.post(`${SECURITY_API}/login`, user)
+    .then(response => response.data);
+
+export const logout = (user) =>
+    api.post(`${SECURITY_API}/logout`, user)
+    .then(response => response.data);
+
+export const profile = () =>
+    api.post(`${SECURITY_API}/profile`)
+    .then(response => response.data);
